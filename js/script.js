6c15d7a5f1f77e07012bbffbb6d8680eef7eea70
Chip Miller <wgmilleriii@gmail.com>
2020-07-12 14:11:57 -0400

    progress..
    progress in grid

--- js/script.js
+++ js/script.js
@@ -1,6 +1,87 @@
 $(function() {
 
 
+  var $fifths_0=["C","G","D","A","E","B","F#","C#","G#","D#","A#","F"];
+  var $fifths_1=["C","F","Bb","Eb","Ab","Db","F#","B","E","A","D","G","C"];
+  var $grid=$("#grid");
+  var $svg=$("#svg1");
+  // sample: https://www.sodawebmedia.com/blog/create-responsive-square-css/
+  // https://www.pagecrafter.com/responsive-centered-square-div-resizes-height/
+  // 
+
+  var $numNotes=12;
+
+  // viewBox width and height
+  var $vb=1800; 
+
+  // width and height percent is 12 divisions
+  var $wh=100/$numNotes;
+
+  // square widths/heights
+  var $padding=2;
+  var $wid=Math.round($vb/($numNotes + $padding));
+
+  for (var $x=0;$x<$numNotes;$x++) {
+    var $s="<div class='gridline'>";
+
+    for (var $y=0;$y<$numNotes;$y++) {
+      
+      var $val=$fifths_0[$y] + " - " + $fifths_1[$x];
+      // x and y points
+      var $px=($x/$numNotes) * $vb;
+      var $py=($y/$numNotes) * $vb;  
+      
+
+      //$s2+=`<rect style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" x="` + $px + `" y="` + $py + `" width="` + $wid + `" height="` + $wid + `"/>`;
+
+      var svgNS="http://www.w3.org/2000/svg";
+      var svg=document.getElementById("svg1");
+      var sq = document.createElementNS(svgNS, 'rect'); //Create a path in SVG's namespace
+      sq.setAttribute("class","lucasSquare"); 
+      sq.setAttribute("x",$px); 
+      sq.setAttribute("y",$py); 
+      sq.setAttribute("width",$wid); 
+      sq.setAttribute("height",$wid); 
+      svg.appendChild(sq);
+
+
+      var txt = document.createElementNS(svgNS, 'text');
+      txt.setAttributeNS(null, 'x', $px + 10);
+      txt.setAttributeNS(null, 'y', $py + 30);
+      txt.setAttributeNS(null,'font-size','30px');
+      txt.innerHTML = $val;
+      svg.appendChild(txt);
+
+
+      // var sq = document.createElementNS("http://www.w3.org/2000/svg", 'text'); //Create a path in SVG's namespace
+      // sq.setAttribute("class","lucasSquareText"); 
+      // sq.setAttribute("x",$px); 
+      // sq.setAttribute("y",$py); 
+      // sq.setAttribute("width",$wid); 
+      // sq.setAttribute("height",$wid); 
+      // svg.appendChild(sq);
+
+
+      // create an HTML div
+      $s+=`<div class="square">
+      <div class="content">
+        ` + $val + `
+      </div>
+    </div>` ;
+
+ 
+    }
+    $s+="</div>";
+
+
+    // add it to the grid
+    $grid.append($s);
+
+  }
+
+
+  // remove the below when done.
+
   // variables - for readability
   var $list, $newItemForm;
 
